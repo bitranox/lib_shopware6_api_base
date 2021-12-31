cb18caceac8856f5ad0ef41e231e904815916432
bitranox <rnowotny1966@gmail.com>
2021-12-31 20:03:40 +0100

    v1.3.1 ------ 2021-12-31: implement testing for python 3.6, 3.7

--- lib_shopware6_api_base/lib_shopware6_api_base_helpers.py
+++ lib_shopware6_api_base/lib_shopware6_api_base_helpers.py
@@ -4,12 +4,6 @@ from typing import Any, Callable
 
 
 def get_pretty_printer() -> Callable[[Any], str]:
-    """
-
-    >>> get_pretty_printer()
-
-    """
-
     # this is only needed for Python3.6, Python3.7
     is_before_python38 = (sys.version_info.major, sys.version_info.minor) < (3, 8)
     if is_before_python38:
