35dce6793d165691264c2353185ad592bba750d6
bitranox <rnowotny1966@gmail.com>
2021-12-31 20:25:16 +0100

    v1.3.1 ------ 2021-12-31: implement testing for python 3.6, 3.7

--- lib_shopware6_api_base/lib_shopware6_api_base_helpers.py
+++ lib_shopware6_api_base/lib_shopware6_api_base_helpers.py
@@ -8,7 +8,8 @@ def get_pretty_printer() -> Callable[[Any], str]:
     is_before_python38 = (sys.version_info.major, sys.version_info.minor) < (3, 8)
     if is_before_python38:
         import pprint3x as pprint  # type: ignore
+        pp = pprint.PrettyPrinter(sort_dicts=False).pprint
     else:
         import pprint  # type: ignore
-    pp = pprint.PrettyPrinter(sort_dicts=False).pprint
+        pp = pprint.PrettyPrinter(sort_dicts=False).pprint
     return pp  # type: ignore
